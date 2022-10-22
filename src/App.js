66ece91cff0e9117268a33233854b3baafa6542d
Georges Sich <danzels5000@gmail.com>
2022-10-22 23:03:02 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import capy from "./images/capy.png";
+import capy2 from './images/capy2.png'
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={capy} alt="capy" />
+      <span>c</span>
+      <span>a</span>
+      <span>r</span>
+      <span>p</span>
+      <span>i</span>
+      <span>n</span>
+      <span>c</span>
+      <span>h</span>
+      <span>o</span>
+      <img src={capy2} alt="capy" />
+      <span>ч</span>
+      <span>е</span>
+      <span>п</span>
+      <span>у</span>
+      <span>б</span>
+      <span>а</span>
+      <span>р</span>
+      <span>а</span>
+      <span></span>
     </div>
   );
 }
