66da813d40c2118da8dca0ddb44fdfe2d9434052
Georges Sich <danzels5000@gmail.com>
2022-10-23 01:53:13 +0300

    third commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,39 @@
 import capy from "./images/capy.png";
-import capy2 from './images/capy2.png'
+import capy2 from "./images/capy2.png";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <img src={capy} alt="capy" />
-      <span>c</span>
-      <span>a</span>
-      <span>r</span>
-      <span>p</span>
-      <span>i</span>
-      <span>n</span>
-      <span>c</span>
-      <span>h</span>
-      <span>o</span>
-      <img src={capy2} alt="capy" />
-      <span>ч</span>
-      <span>е</span>
-      <span>п</span>
-      <span>у</span>
-      <span>б</span>
-      <span>а</span>
-      <span>р</span>
-      <span>а</span>
-      <span></span>
+      <div>
+        <img className="capy" src={capy} alt="capy" />
+        <div>
+          <span>c</span>
+          <span>a</span>
+          <span>r</span>
+          <span>p</span>
+          <span>i</span>
+          <span>n</span>
+          <span>c</span>
+          <span>h</span>
+          <span>o</span>
+        </div>
+      </div>
+      <div>
+        <img className="capy-2" src={capy2} alt="capy" />
+        <div>
+          <span>ч</span>
+          <span>е</span>
+          <span>п</span>
+          <span>у</span>
+          <span>б</span>
+          <span>а</span>
+          <span>р</span>
+          <span>а</span>
+          <span></span>
+        </div>
+        
+      </div>
     </div>
   );
 }
