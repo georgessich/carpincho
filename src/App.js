d3ea6d5746e765fe9ef6412555b2d602ab57af35
Georges Sich <danzels5000@gmail.com>
2022-10-26 21:11:10 +0300

    new concept

--- src/App.js
+++ src/App.js
@@ -1,39 +1,63 @@
-import capy from "./images/capy.png";
-import capy2 from "./images/capy2.png";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+import capy from "./images/capy.jpg";
+import beaver from "./images/beav.jpg";
+import cat from "./images/cat.png";
+import otter from "./images/otter.jpg";
+import capycoin from "./images/capycoin.png";
+import beaver2 from "./images/beav2.jpg";
+import { Reorder } from "framer-motion";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <div>
-        <img className="capy" src={capy} alt="capy" />
-        <div>
-          <span>c</span>
-          <span>a</span>
-          <span>r</span>
-          <span>p</span>
-          <span>i</span>
-          <span>n</span>
-          <span>c</span>
-          <span>h</span>
-          <span>o</span>
-        </div>
-      </div>
-      <div>
-        <img className="capy-2" src={capy2} alt="capy" />
-        <div>
-          <span>ч</span>
-          <span>е</span>
-          <span>п</span>
-          <span>у</span>
-          <span>б</span>
-          <span>а</span>
-          <span>р</span>
-          <span>а</span>
-          <span></span>
-        </div>
-        
-      </div>
+      <Parallax pages={2}>
+        <ParallaxLayer
+          speed={0.5}
+          factor={1.5}
+          offset={0}
+          style={{
+            backgroundImage: `url(${capy})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center center",
+          }}
+        >
+          <p className="title">CARPINCHO COIN</p>
+          <p className="subtitle">
+            Гиперсовременная децентрализированная валюта основанная на
+            уникальной технологии CapyChain™®
+          </p>
+
+          {/* <Reorder.Group axis="y" onReorder={setInitItems} values={initItems}>
+            {initItems.map(({ title, text, i }) => (
+              <Accordion key={i} title={title} text={text} />
+             
+            ))}
+          </Reorder.Group> */}
+        </ParallaxLayer>
+        <ParallaxLayer
+          speed={0.5}
+          factor={1.5}
+          offset={1}
+          style={{
+            // backgroundImage: `url(${beaver})`,
+            // backgroundSize: "cover",
+            // backgroundPosition: "center center",
+            backgroundColor: "rgb(100 90 141)",
+          }}
+        >
+         
+          <div className="capy__container">
+          <img src={capycoin} alt="capycoin" className="capy__container-coin" />
+          <span className="capy__container-text">
+          Carpincho - это валюта со стабильной ценой на базе CapyChain™®, выпуском и развитием которого управляют Melon Protocol и децентрализованая автономная организация Carpincho LLC.
+          </span>
+          </div>
+          
+        </ParallaxLayer>
+       
+
+      </Parallax>
     </div>
   );
 }
